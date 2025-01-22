5d1ff36e5f0bcad55762107b241be8070730cd43
PalibrixBeet <pavlo.semeniuk@beetroot.se>
2025-01-22 12:57:03 +0200

    Update merger_additional_fields.py
    Changed fuzzy logic for Levenshtein's distance

--- merger_additional_fields.py
+++ merger_additional_fields.py
@@ -10,6 +10,8 @@ import pathlib
 import warnings
 from datetime import time
 
+# import Levenshtein
+from Levenshtein import distance
 import numpy as np
 import pandas as pd
 import progressbar
@@ -129,7 +131,7 @@ def process_files(filenames, similarity_score):
 
         frame_name = re.sub(r'[^\p{L}\p{N}]+', '', unidecode(from_second_df_frame[0]).lower()).replace(' ', '') \
             if isinstance(from_second_df_frame[0], str) else 'No_name'  # only letters from name
-        match_frame_name = [f_name for f_name in presentations_names_list_firs_df if fuzz.ratio(f_name, frame_name) > 95 ]
+        match_frame_name = [f_name for f_name in presentations_names_list_firs_df if distance(f_name, frame_name) <= 3]
         main_frame_index = presentations_names_list_firs_df.index(match_frame_name[0]) if match_frame_name else int()
 
         if match_frame_name and frame_name != 'No_name':
